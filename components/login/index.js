c9a0ba626accdf13173464ea8bcedf54b6027f8f
Paladiya <mrpaladiyagautam@gmail.com>
2021-01-24 16:02:59 +0100

    Fix login bug

--- components/login/index.js
+++ components/login/index.js
@@ -21,7 +21,7 @@ export default function LoginComponent() {
   const [user, setUser] = useState(INITIAL_USER);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
-  const re = '/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/';
+  const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
   const handleSubmit = (event) => {
     event.preventDefault();
